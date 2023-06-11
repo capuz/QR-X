40314de839046c44a17bdf6b481d2416fc0f196e
fcapuz <fcapuz@DESKTOP-1MR6HL2>
2023-06-10 20:17:55 -0400

    Add project files.

---
+++ Controllers/QrTestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QrMvc.Controllers
+{
+    public class QrTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TestAdminLTE()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QrMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
